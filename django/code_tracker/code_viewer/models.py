d9b6bd3211e8170b0a1c49047b155a673df309e4
nyxcharon <nyxcharon@gmail.com>
2014-04-24 15:20:29 -0400

    updating model for code_viewer

--- django/code_tracker/code_viewer/models.py
+++ django/code_tracker/code_viewer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Repo(models.Model):
+    repo_type =models.CharField(max_length=3)
+    repo_url = models.CharField(max_length=300)
+    repo_name = models.CharField(max_length=20)
+    repo_location = models.CharField(max_length=50)
