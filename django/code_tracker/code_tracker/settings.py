2f202220f2bc177ac0b71959fad748c8cdff017e
nyxcharon <nyxcharon@gmail.com>
2014-04-25 22:23:24 -0400

    Added basic template that uses bootstrap

--- django/code_tracker/code_tracker/settings.py
+++ django/code_tracker/code_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'code_viewer',
+    'bootstrap3',
 )
 
 MIDDLEWARE_CLASSES = (
--- django/code_tracker/code_tracker/urls.py
+++ django/code_tracker/code_tracker/urls.py
@@ -5,8 +5,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'code_tracker.views.home', name='home'),
+    #url(r'^$', 'code_tracker.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('code_viewer.urls')),                     
+    url(r'^^repo/', include('code_viewer.urls')),                     
     url(r'^admin/', include(admin.site.urls)),
+
 )
