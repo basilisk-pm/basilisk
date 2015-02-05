38d7941cb5678f3e0a6240b861379e471bab204d
nyxcharon <nyxcharon@gmail.com>
2015-02-05 12:35:59 -0500

    adding project navbar

--- basilisk_pm/basilisk_pm/urls.py
+++ basilisk_pm/basilisk_pm/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+import projects
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'basilisk_pm.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', include('projects.urls')),
+    url(r'^$', TemplateView.as_view(template_name='base.html')),
     url(r'^projects/', include('projects.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
