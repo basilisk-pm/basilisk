2a7e9ad060b3bae8bf55e43ff008285a701edb45
nyxcharon <nyxcharon@gmail.com>
2015-02-05 12:10:00 -0500

    Setting up template inheritance

--- basilisk_pm/basilisk_pm/urls.py
+++ basilisk_pm/basilisk_pm/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'basilisk_pm.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', include('projects.urls')),
     url(r'^projects/', include('projects.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- basilisk_pm/projects/views.py
+++ basilisk_pm/projects/views.py
@@ -4,6 +4,6 @@ from django.template import RequestContext, loader
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('base.html')
+    template = loader.get_template('project.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
