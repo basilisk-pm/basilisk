31c1aead8c77aebe08f7838778fdb0358fd3135f
nyxcharon <nyxcharon@gmail.com>
2015-02-05 13:53:41 -0500

    Updating project list page

--- basilisk_pm/basilisk_pm/urls.py
+++ basilisk_pm/basilisk_pm/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'basilisk_pm.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^$', TemplateView.as_view(template_name='project-list.html')),
     url(r'^projects/', include('projects.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
