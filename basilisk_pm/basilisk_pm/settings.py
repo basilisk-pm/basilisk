d34a241caf8fe84be4b5f4f1ea73fec58ecbe576
nyxcharon <nyxcharon@gmail.com>
2015-02-07 22:21:34 -0500

    User registration working

--- basilisk_pm/basilisk_pm/settings.py
+++ basilisk_pm/basilisk_pm/settings.py
@@ -56,7 +56,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
-    'projects'
+    'projects',
+    'profiles'
 )
 
 MIDDLEWARE_CLASSES = (
--- basilisk_pm/basilisk_pm/urls.py
+++ basilisk_pm/basilisk_pm/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-import projects
+import projects, profiles
+
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,6 +13,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='project-list.html')),
     url(r'^projects/', include('projects.urls')),
+    url(r'^profiles/', include('profiles.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
