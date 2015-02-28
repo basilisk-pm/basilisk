d8f17f2150eecbe034b9627a6e33234fd0a70489
nyxcharon <nyxcharon@gmail.com>
2015-02-27 21:15:25 -0500

    Adding project to admin page

--- basilisk_pm/projects/models.py
+++ basilisk_pm/projects/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+#from profiles.models import UserProfile
 
 # Create your models here.
 class Project(models.Model):
     proj_name = models.CharField(max_length=20)
-    owner = models.CharField(max_length=10)
-    pub_date = models.DateTimeField('Date Published')
+    owner  = models.ForeignKey('profiles.UserProfile')
+    pub_date = models.DateTimeField(auto_now=True)
+    #models.DateTimeField('Date Published')
     proj_desc = models.CharField(max_length=200)
+
+
+    def __unicode__(self):
+        return self.proj_name
