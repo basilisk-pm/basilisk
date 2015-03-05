08088c5b7cc9c1f0428e77c04696c353c7c99b79
nyxcharon <nyxcharon@gmail.com>
2015-03-05 16:48:19 -0500

    Adding projects to admin page

--- basilisk_pm/projects/admin.py
+++ basilisk_pm/projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from projects.models import Project 
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('proj_name', 'owner', 'pub_date')
+
+admin.site.register(Project,ProjectAdmin)
+
