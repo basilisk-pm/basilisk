66e7a6649f55f1a2f9985362a496000ef1c86473
nyxcharon <nyxcharon@gmail.com>
2015-04-14 17:24:24 -0400

    Adding new file model to project

--- basilisk_pm/projects/models.py
+++ basilisk_pm/projects/models.py
@@ -1,14 +1,23 @@
 from django.db import models
-#from profiles.models import UserProfile
 
-# Create your models here.
+"""Define the project model"""
 class Project(models.Model):
     proj_name = models.CharField(max_length=20)
     owner  = models.ForeignKey('profiles.UserProfile')
     pub_date = models.DateTimeField(auto_now=True)
-    #models.DateTimeField('Date Published')
     proj_desc = models.CharField(max_length=200)
     git_url = models.URLField(blank=True)
 
     def __unicode__(self):
         return self.proj_name
+
+"""Generate the path for the file to be uploaded to"""
+def generate_filename(instance,filename):
+    return "static/project-files/%s/%s" % (instance.user.username, filename)
+
+"""Model that assoctiates a file with a specific project"""
+class ProjectFile(models.Model):
+    project = models.ForeignKey(Project)
+    owner = models.ForeignKey('profiles.UserProfile')
+    pub_date = models.DateTimeField(auto_now=True)
+    pfile = models.FileField(upload_to=generate_filename)
