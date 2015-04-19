f34104e8b6c1010f97c570a2f4078fcbd87dcc45
nyxcharon <nyxcharon@gmail.com>
2015-04-18 20:25:01 -0400

    Initial working comment system for bugs. Needs css added

--- basilisk_pm/bugs/forms.py
+++ basilisk_pm/bugs/forms.py
@@ -1,9 +1,13 @@
 from django.forms import ModelForm
-from bugs.models import Bug
+from bugs.models import Bug,BugComment
 
 class BugForm(ModelForm):
     class Meta:
         model = Bug
         fields = ['name','description']
 
+class BugCommentForm(ModelForm):
+    class Meta:
+        model = BugComment
+        fields = ['comment']
 
--- basilisk_pm/bugs/views.py
+++ basilisk_pm/bugs/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.contrib.auth.decorators import login_required
 from projects.models import Project
-from bugs.forms import BugForm
-from bugs.models import Bug
+from bugs.forms import BugForm, BugCommentForm
+from bugs.models import Bug, BugComment
 import datetime
 import operator
 
@@ -56,7 +56,27 @@ def view_issue(request,project_id,bug_id):
     template = loader.get_template('bugs-view.html')
     context = RequestContext(request, {})
     context['project'] = get_object_or_404(Project,pk=project_id)
-    context['bug'] = get_object_or_404(Bug,pk=bug_id)
-    #data = Bug.objects.filter(project=project_id)
-    #context['issues'] = sorted(data, key=operator.attrgetter('created_date'), reverse=True)[:3]
+    current_bug = get_object_or_404(Bug,pk=bug_id)
+    created = False
+
+    if request.method == 'POST':
+        form = BugCommentForm(request.POST)
+        if form.is_valid():
+            bug_form = form.save(commit=False)
+            bug_form.creator = request.user
+            bug_form.project = get_object_or_404(Project,pk=project_id)
+            bug_form.created_date = datetime.datetime.now()
+            bug_form.bug = current_bug
+            bug_form.save()
+            created = True
+        else:
+            print form.errors
+    else:
+       form = BugCommentForm()
+    
+    comments = BugComment.objects.filter(bug=current_bug)
+    context['comments'] = sorted(comments, key=operator.attrgetter('created_date'))
+    context['bug'] = current_bug
+    context['bug_form'] = form
+    context['created'] = created
     return HttpResponse(template.render(context))
